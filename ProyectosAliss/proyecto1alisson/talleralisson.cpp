a5ff186320777cde039eaa9d608c357aa1436f64
Alisson Hernandez <anhernandez992@soy.sena.edu.co>
2025-05-27 07:13:52 -0500

    Add files via upload

---
+++ ProyectosAliss/proyecto1alisson/talleralisson.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userInput;
+    cout << "12. Ingresa un número: ";
+    cin >> userInput;
+    cout << "Ingresaste: " << userInput << endl;
+    return 0;
+}
